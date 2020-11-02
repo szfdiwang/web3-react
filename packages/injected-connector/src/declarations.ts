8c4da871de2f4c5177edce3d7bab3ddd660d8115
wcc <1726021@qq.com>
2020-11-02 13:52:57 +0800

    修改eth到alaya

--- packages/injected-connector/src/declarations.ts
+++ packages/injected-connector/src/declarations.ts
@@ -1,4 +1,4 @@
-interface Ethereum {
+interface Alaya {
   send: unknown
   enable: () => Promise<string[]>
   on?: (method: string, listener: (...args: any[]) => void) => void
@@ -6,7 +6,7 @@ interface Ethereum {
 }
 
 declare interface Window {
-  ethereum?: Ethereum
+  alaya?: Alaya
 }
 
 declare const __DEV__: boolean
--- packages/injected-connector/src/index.ts
+++ packages/injected-connector/src/index.ts
@@ -8,11 +8,11 @@ function parseSendReturn(sendReturn: SendReturnResult | SendReturn): any {
   return sendReturn.hasOwnProperty('result') ? sendReturn.result : sendReturn
 }
 
-export class NoEthereumProviderError extends Error {
+export class NoAlayaProviderError extends Error {
   public constructor() {
     super()
     this.name = this.constructor.name
-    this.message = 'No Ethereum provider was found on window.ethereum.'
+    this.message = 'No Alaya provider was found on window.alaya.'
   }
 }
 
@@ -38,7 +38,7 @@ export class InjectedConnector extends AbstractConnector {
     if (__DEV__) {
       console.log("Handling 'chainChanged' event with payload", chainId)
     }
-    this.emitUpdate({ chainId, provider: window.ethereum })
+    this.emitUpdate({ chainId, provider: window.alaya })
   }
 
   private handleAccountsChanged(accounts: string[]): void {
@@ -63,29 +63,29 @@ export class InjectedConnector extends AbstractConnector {
     if (__DEV__) {
       console.log("Handling 'networkChanged' event with payload", networkId)
     }
-    this.emitUpdate({ chainId: networkId, provider: window.ethereum })
+    this.emitUpdate({ chainId: networkId, provider: window.alaya})
   }
 
   public async activate(): Promise<ConnectorUpdate> {
-    if (!window.ethereum) {
-      throw new NoEthereumProviderError()
+    if (!window.alaya) {
+      throw new NoAlayaProviderError()
     }
 
-    if (window.ethereum.on) {
-      window.ethereum.on('chainChanged', this.handleChainChanged)
-      window.ethereum.on('accountsChanged', this.handleAccountsChanged)
-      window.ethereum.on('close', this.handleClose)
-      window.ethereum.on('networkChanged', this.handleNetworkChanged)
+    if (window.alaya.on) {
+      window.alaya.on('chainChanged', this.handleChainChanged)
+      window.alaya.on('accountsChanged', this.handleAccountsChanged)
+      window.alaya.on('close', this.handleClose)
+      window.alaya.on('networkChanged', this.handleNetworkChanged)
     }
 
-    if ((window.ethereum as any).isMetaMask) {
-      ;(window.ethereum as any).autoRefreshOnNetworkChange = false
+    if ((window.alaya as any).isMetaMask) {
+      ;(window.alaya as any).autoRefreshOnNetworkChange = false
     }
 
     // try to activate + get account via eth_requestAccounts
     let account
     try {
-      account = await (window.ethereum.send as Send)('eth_requestAccounts').then(
+      account = await (window.alaya.send as Send)('eth_requestAccounts').then(
         sendReturn => parseSendReturn(sendReturn)[0]
       )
     } catch (error) {
@@ -98,31 +98,31 @@ export class InjectedConnector extends AbstractConnector {
     // if unsuccessful, try enable
     if (!account) {
       // if enable is successful but doesn't return accounts, fall back to getAccount (not happy i have to do this...)
-      account = await window.ethereum.enable().then(sendReturn => sendReturn && parseSendReturn(sendReturn)[0])
+      account = await window.alaya.enable().then(sendReturn => sendReturn && parseSendReturn(sendReturn)[0])
     }
 
-    return { provider: window.ethereum, ...(account ? { account } : {}) }
+    return { provider: window.alaya, ...(account ? { account } : {}) }
   }
 
   public async getProvider(): Promise<any> {
-    return window.ethereum
+    return window.alaya
   }
 
   public async getChainId(): Promise<number | string> {
-    if (!window.ethereum) {
-      throw new NoEthereumProviderError()
+    if (!window.alaya) {
+      throw new NoAlayaProviderError()
     }
 
     let chainId
     try {
-      chainId = await (window.ethereum.send as Send)('eth_chainId').then(parseSendReturn)
+      chainId = await (window.alaya.send as Send)('eth_chainId').then(parseSendReturn)
     } catch {
       warning(false, 'eth_chainId was unsuccessful, falling back to net_version')
     }
 
     if (!chainId) {
       try {
-        chainId = await (window.ethereum.send as Send)('net_version').then(parseSendReturn)
+        chainId = await (window.alaya.send as Send)('net_version').then(parseSendReturn)
       } catch {
         warning(false, 'net_version was unsuccessful, falling back to net version v2')
       }
@@ -130,21 +130,21 @@ export class InjectedConnector extends AbstractConnector {
 
     if (!chainId) {
       try {
-        chainId = parseSendReturn((window.ethereum.send as SendOld)({ method: 'net_version' }))
+        chainId = parseSendReturn((window.alaya.send as SendOld)({ method: 'net_version' }))
       } catch {
         warning(false, 'net_version v2 was unsuccessful, falling back to manual matches and static properties')
       }
     }
 
     if (!chainId) {
-      if ((window.ethereum as any).isDapper) {
-        chainId = parseSendReturn((window.ethereum as any).cachedResults.net_version)
+      if ((window.alaya as any).isDapper) {
+        chainId = parseSendReturn((window.alaya as any).cachedResults.net_version)
       } else {
         chainId =
-          (window.ethereum as any).chainId ||
-          (window.ethereum as any).netVersion ||
-          (window.ethereum as any).networkVersion ||
-          (window.ethereum as any)._chainId
+          (window.alaya as any).chainId ||
+          (window.alaya as any).netVersion ||
+          (window.alaya as any).networkVersion ||
+          (window.alaya as any)._chainId
       }
     }
 
@@ -152,48 +152,48 @@ export class InjectedConnector extends AbstractConnector {
   }
 
   public async getAccount(): Promise<null | string> {
-    if (!window.ethereum) {
-      throw new NoEthereumProviderError()
+    if (!window.alaya) {
+      throw new NoAlayaProviderError()
     }
 
     let account
     try {
-      account = await (window.ethereum.send as Send)('eth_accounts').then(sendReturn => parseSendReturn(sendReturn)[0])
+      account = await (window.alaya.send as Send)('eth_accounts').then(sendReturn => parseSendReturn(sendReturn)[0])
     } catch {
       warning(false, 'eth_accounts was unsuccessful, falling back to enable')
     }
 
     if (!account) {
       try {
-        account = await window.ethereum.enable().then(sendReturn => parseSendReturn(sendReturn)[0])
+        account = await window.alaya.enable().then(sendReturn => parseSendReturn(sendReturn)[0])
       } catch {
         warning(false, 'enable was unsuccessful, falling back to eth_accounts v2')
       }
     }
 
     if (!account) {
-      account = parseSendReturn((window.ethereum.send as SendOld)({ method: 'eth_accounts' }))[0]
+      account = parseSendReturn((window.alaya.send as SendOld)({ method: 'eth_accounts' }))[0]
     }
 
     return account
   }
 
   public deactivate() {
-    if (window.ethereum && window.ethereum.removeListener) {
-      window.ethereum.removeListener('chainChanged', this.handleChainChanged)
-      window.ethereum.removeListener('accountsChanged', this.handleAccountsChanged)
-      window.ethereum.removeListener('close', this.handleClose)
-      window.ethereum.removeListener('networkChanged', this.handleNetworkChanged)
+    if (window.alaya && window.alaya.removeListener) {
+      window.alaya.removeListener('chainChanged', this.handleChainChanged)
+      window.alaya.removeListener('accountsChanged', this.handleAccountsChanged)
+      window.alaya.removeListener('close', this.handleClose)
+      window.alaya.removeListener('networkChanged', this.handleNetworkChanged)
     }
   }
 
   public async isAuthorized(): Promise<boolean> {
-    if (!window.ethereum) {
+    if (!window.alaya) {
       return false
     }
 
     try {
-      return await (window.ethereum.send as Send)('eth_accounts').then(sendReturn => {
+      return await (window.alaya.send as Send)('eth_accounts').then(sendReturn => {
         if (parseSendReturn(sendReturn).length > 0) {
           return true
         } else {
